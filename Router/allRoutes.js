f87ec2108d7fcc996b876a7214de4da3faeeb42b
Kévin <xdarkcaim@gmail.com>
2025-11-21 19:06:21 +0100

    Update 404 page link, add account and password edit pages, enhance gallery and signup forms, and improve CSS styling

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,13 +2,16 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),
-/*    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/carte", "La carte", "/pages/carte.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
-    new Route("/account", "Mon compte", "/pages/account.html"),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/editPassword.html"),*/
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/galerie", "La galerie", "/pages/galerie.html"),
+  new Route("/signin", "connexion", "/pages/signin.html"),
+  new Route("/signup", "inscription", "/pages/signup.html"),
+  new Route("/account", "mon compte", "/pages/account.html"),
+  new Route(
+    "/editpassword",
+    "modifier mot de passe",
+    "/pages/editpassword.html",
+  ),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
